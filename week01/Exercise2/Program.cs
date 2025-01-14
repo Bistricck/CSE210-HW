50bca3fb47f952be9933b5390fee5e268e089b63
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-14 10:09:42 -0800

    exercise 2 changing the letter grades.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grading percentage? ");
+        string ValueFromUser = Console.ReadLine();
+
+        int x = int.Parse(ValueFromUser);
+
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else if (x <= 59)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your letter grade is: {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the class! ");
+        }
+
+        else
+        {
+            Console.WriteLine("I am sorry, you did not pass the class. Try again!");
+        }
     }
 }
