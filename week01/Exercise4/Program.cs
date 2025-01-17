1f001f4a0d6853e5ddd8d010688720ef5da2effc
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-17 09:21:20 -0800

    Week four exercise completed.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,12 +7,12 @@ class Program
         List<int> numbers = new List<int>();
 
         int number;
-        int sum =0;
+        int sum = 0;
 
-
+        
         do
         {
-            Console.Write("Enter any number, 0 to stop: ");
+            Console.Write("Enter any number. When you are finished enter 0 to stop the program: ");
             string input = Console.ReadLine();
 
             // Parse the input safely
@@ -21,6 +21,7 @@ class Program
                 if (number != 0)
                 {
                 numbers.Add(number);
+                sum += number;
                 }
             }
 
@@ -31,18 +32,39 @@ class Program
 
         } while (number != 0);
 
-        if (number.Count == 0)
+        if (numbers.Count == 0)
             {
                 Console.WriteLine("No numbers were entered.");
             }
-
             else 
             {
-                Console.WriteLine("The sum of the numbers you entered is: ");
-                foreach (int num in numbers)
+            Console.WriteLine("The numbers you have entered are: ");
+            foreach (int num in numbers)
             {
             Console.WriteLine(num);
             }
+            
+            Console.WriteLine($"The sum of the numbers you entered is {sum}. ");
+            
+            double average = (double)sum / numbers.Count;
+            {
+                Console.WriteLine($"The average is {average}");
+            }
+
+            int max = numbers [0];
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            
+            }
+            {
+                Console.WriteLine($"The largest number is {max}.");
+            }
+
 
             }
 
