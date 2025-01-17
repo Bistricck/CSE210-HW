ebb5e27b1510ab3760f6d0ec018c5a4f6a884c05
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-17 00:39:15 -0800

    Exercise 4. incomplete.. updating current progress

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int number;
+        int sum =0;
+
+
+        do
+        {
+            Console.Write("Enter any number, 0 to stop: ");
+            string input = Console.ReadLine();
+
+            // Parse the input safely
+            if (int.TryParse(input, out number))
+            {
+                if (number != 0)
+                {
+                numbers.Add(number);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input, please enter a valid number. ");
+            }
+
+        } while (number != 0);
+
+        if (number.Count == 0)
+            {
+                Console.WriteLine("No numbers were entered.");
+            }
+
+            else 
+            {
+                Console.WriteLine("The sum of the numbers you entered is: ");
+                foreach (int num in numbers)
+            {
+            Console.WriteLine(num);
+            }
+
+            }
+
     }
 }
