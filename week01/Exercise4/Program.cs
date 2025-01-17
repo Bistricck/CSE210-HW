8abac11802d76a006e8367ae2ef5f0d9fbfc7188
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-17 09:34:34 -0800

    final additions made to program including explanations for what is being done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Create a list to store the numbers entered by the user.
         List<int> numbers = new List<int>();
 
         int number;
@@ -12,12 +13,14 @@ class Program
         
         do
         {
+            //Prompt the user to enter a number.
             Console.Write("Enter any number. When you are finished enter 0 to stop the program: ");
             string input = Console.ReadLine();
 
             // Parse the input safely
             if (int.TryParse(input, out number))
             {
+                //Add the number to the list and update the sum if its not 0.
                 if (number != 0)
                 {
                 numbers.Add(number);
@@ -27,6 +30,7 @@ class Program
 
             else
             {
+                //inform the user that the input must be valid
                 Console.WriteLine("Invalid input, please enter a valid number. ");
             }
 
@@ -34,23 +38,29 @@ class Program
 
         if (numbers.Count == 0)
             {
+                //inform the user that no numbers were entered
                 Console.WriteLine("No numbers were entered.");
             }
             else 
             {
+                //Display the numbers entered by the user.
             Console.WriteLine("The numbers you have entered are: ");
             foreach (int num in numbers)
             {
             Console.WriteLine(num);
             }
-            
+            //Display the sum of the numbers entered by the user.
             Console.WriteLine($"The sum of the numbers you entered is {sum}. ");
             
+
+            //calculate and display the average of the numbers.
             double average = (double)sum / numbers.Count;
+            
             {
                 Console.WriteLine($"The average is {average}");
             }
 
+            //Find the largest number.
             int max = numbers [0];
 
             foreach (int num in numbers)
@@ -62,6 +72,7 @@ class Program
             
             }
             {
+                //Display to 
                 Console.WriteLine($"The largest number is {max}.");
             }
 
