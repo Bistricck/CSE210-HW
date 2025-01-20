294fb534e20ecabff1349ada794b4291d03083b9
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-20 00:08:50 -0800

    class resume

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,13 @@
+namespace Journal;
+
+public class Journal
+{
+    public List<string> dailyPromts = ["What are you grateful for today?",
+                                        "What was the most interesting thing that happened today?",
+                                        "How were you kind to other people today?",
+                                        "What weird thing did you see today?",
+                                        "What did you talk about today with a friend?",
+                                        "What did the spirit tell me today?"];
+    public string promptgenerator 
+                                    
+}
