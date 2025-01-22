9d009b06e5ffcd4e87f6cdce9f646a6dbdc61356
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-22 01:25:19 -0800

    updated entry class

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,11 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Entry
 {
-    string _date;
-    string _promptText;
-    string _entryText;
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public Entry(string date, string promptText, string entryText)
+    {
+        _date = date;
+        _promptText = promptText;
+        _entryText = entryText;
+    }
 
     public void Display()
     {
+        Console.WriteLine($"Date; {_date}");
+        Console.WriteLine($"prompt: {_promptText}");
+        Console.WriteLine($"Entry: {_entryText}");
+    }
 
+    public string ToCsv()
+    {
+        return ($"{_date}, {_promptText}, {_entryText}");
+    }
+
+
+    public static Entry FromCsv(string csvLine)
+    {
+        var parts = csvLine.Split(',');
+        if (parts.Length >= 3 )
+        {
+            return new Entry(parts[0], parts[1], parts[2]);
+        }
+        throw new FormatException("Invalid CSV format for Entry.");
     }
 }
