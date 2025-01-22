933f9162fdba48ea92093cb8698146e368e45759
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-22 01:08:44 -0800

    updated list

--- week02/Journal/Promptgenerator.cs
+++ week02/Journal/Promptgenerator.cs
@@ -1,16 +1,20 @@
 public class Promptgenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>
+    {
+        "What are you grateful for today?",
+        "What was the most interesting thing that happened today?",
+        "How were you kind to other people today?",
+        "What weird thing did you see today?",
+        "What did you talk about today with a friend?",
+        "What did the spirit tell me today?"
+    };
 
     public string GetRandomPrompt()
     {
-        return "";
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
     }
 
 }
-// //["What are you grateful for today?",
-//                                         "What was the most interesting thing that happened today?",
-//                                         "How were you kind to other people today?",
-//                                         "What weird thing did you see today?",
-//                                         "What did you talk about today with a friend?",
-//                                         "What did the spirit tell me today?"];
