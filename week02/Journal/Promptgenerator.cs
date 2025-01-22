08a435dd38c0292b0859466dd00cc77bdfad4909
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-01-22 00:01:50 -0800

    promptgenerator class created

---
+++ week02/Journal/Promptgenerator.cs
@@ -0,0 +1,16 @@
+public class Promptgenerator
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+}
+// //["What are you grateful for today?",
+//                                         "What was the most interesting thing that happened today?",
+//                                         "How were you kind to other people today?",
+//                                         "What weird thing did you see today?",
+//                                         "What did you talk about today with a friend?",
+//                                         "What did the spirit tell me today?"];
