d9a31ff204a347fb010d6c0fd58e25fbeef27af9
Joseph Trumbo <josephdtrumbo10@gmail.com>
2025-02-17 16:08:56 -0800

    Eternal Quest Program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+ GoalManager manager = new GoalManager();
+        manager.LoadGoals();
+
+        while (true)
+        {
+            Console.WriteLine("\n1. Display Score\n2. List Goals\n3. Create Goal\n4. Record Event\n5. Save & Exit");
+            Console.Write("Select an option: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1) manager.DisplayPlayerInfo();
+            else if (choice == 2) manager.ListGoalNames();
+            else if (choice == 3) manager.CreateGoal();
+            else if (choice == 4) manager.RecordEvent();
+            else if (choice == 5) { manager.SaveGoals(); break; }
+        }
     }
 }
